6a93461065511b169faf5fd198e6b3e07bcafede
David Meijer <davidmeijer282@gmail.com>
2018-06-04 10:20:56 +0200

    Commit

---
+++ Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace ZuulCS
+{
+    public class Player
+
+    {
+        private Room currentRoom;
+        private uint damage;
+        private uint health;
+        private Boolean isAlive;
+
+        public uint Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                if (health <= 0)
+                {
+                    Console.WriteLine("You died");
+                }
+            }
+        }
+
+        public Player()
+        {
+            isAlive = true;
+            damage = 2;
+            health = 10;
+        
+        }
+
+        public void Update()
+        {
+            if (health <= 0)
+            {
+                isAlive = false;
+            }
+            if (isAlive == false)
+            {
+                
+            }
+        }
+
+        
+
+
+    }
+}
